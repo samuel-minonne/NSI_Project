55eda4f1839c001cc3419b472503cf1c7992f817
Samuel Minonne <79083644+samuel-minonne@users.noreply.github.com>
2022-12-19 12:15:02 +0100

    got rid of the placeholder code, now we can run the game

--- main.py
+++ main.py
@@ -1,53 +1,23 @@
 import pyxel
+import hitboxes
+import gameobjects
+import json
 
-class Jeu:
-    def __init__(self):
+class Room:
+    """A room, handles the running of the actual game"""
+    def __init__(self) -> None:
+        """Creates a Room"""
+        self.player = gameobjects.Player(0,0)#The player
+        self.walls_hitboxes = [] #All the hitboxes of the walls
+        
+room = Room()
+pyxel.init(128, 128, title="Protoknight", fps= 60)
 
-        # taille de la fenetre 128x128 pixels
-        # ne pas modifier
-        pyxel.init(128, 128, title="Nuit du c0de")
-
-        # position initiale du vaisseau
-        # (origine des positions : coin haut gauche)
-        self.vaisseau_x = 60
-        self.vaisseau_y = 60
-
-        pyxel.run(self.update, self.draw)
-
-
-    def vaisseau_deplacement(self):
-        """déplacement avec les touches de directions"""
-
-        if pyxel.btn(pyxel.KEY_RIGHT) and self.vaisseau_x<120:
-            self.vaisseau_x += 1
-        if pyxel.btn(pyxel.KEY_LEFT) and self.vaisseau_x>0:
-            self.vaisseau_x += -1
-        if pyxel.btn(pyxel.KEY_DOWN) and self.vaisseau_y<120:
-            self.vaisseau_y += 1
-        if pyxel.btn(pyxel.KEY_UP) and self.vaisseau_y>0:
-            self.vaisseau_y += -1
-
-
-    # =====================================================
-    # == UPDATE
-    # =====================================================
-    def update(self):
-        """mise à jour des variables (30 fois par seconde)"""
-
-        # deplacement du vaisseau
-        self.vaisseau_deplacement()
-
-
-    # =====================================================
-    # == DRAW
-    # =====================================================
-    def draw(self):
-        """création et positionnement des objets (30 fois par seconde)"""
-
-        # vide la fenetre
-        pyxel.cls(0)
-
-        # vaisseau (carre 8x8)
-        pyxel.rect(self.vaisseau_x, self.vaisseau_y, 8, 8, 1)
-
-Jeu()
+def update():
+    room.player.update()
+    
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(room.player.xpos,room.player.ypos,room.player.length,room.player.height,3)
+    
+pyxel.run(update, draw)
