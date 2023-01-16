264e9eff02f3a128106aa608034f737a4845461f
vhuber-sama <117632632+vhuber-sama@users.noreply.github.com>
2023-01-16 08:52:20 +0100

    added the list that says where are the stage hitboxes

---
+++ list_for_hitboxes.py
@@ -0,0 +1,8 @@
+list_wallHitboxes = [[0,0,8,8],[16,8,32,8]]
+x = list_wallHitboxes[0[0]]
+y = list_wallHitboxes-[0[1]]
+h = list_wallHitboxes[0[2]]
+l = list_wallHitboxes[0[3]]
+
+for h in list_wallHitboxes:
+    wallhitbox.appendHitbox(h[0],h[1],h[2],h[3])
