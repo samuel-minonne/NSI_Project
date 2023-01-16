c0260ddffd588b2fe56c79a2114a8c7d72fcc421
vhuber-sama <117632632+vhuber-sama@users.noreply.github.com>
2023-01-16 09:00:35 +0100

    Delete list_for_hitboxes1.py

--- list_for_hitboxes1.py
+++
@@ -1,9 +0,0 @@
-list_wallHitboxes = [[0,0,8,8],[16,8,32,8]]
-
-
-for h in list_wallHitboxes:
-    x = list_wallHitboxes[h][0]
-    y = list_wallHitboxes-[h][1]
-    height = list_wallHitboxes[h][2]
-    l = list_wallHitboxes[h][3]
-    wallhitbox.appendHitbox(h[0],h[1],h[2],h[3])
