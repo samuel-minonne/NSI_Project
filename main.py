ec3396b4345fe59da4ffcfa90595a44e54e6ba77
Elisa N <117632231+iminiom@users.noreply.github.com>
2023-02-02 11:32:51 +0100

    added death message

--- main.py
+++ main.py
@@ -34,14 +34,22 @@ def update():
     for e in room.enemies_list:
         e.update()
     camera.focusOn(room.player.xpos+4,room.player.ypos+4)
-    
+
+    if room.player.ypos > 250:
+        room.player.hp = 0
+
+
 def draw():
     pyxel.cls(0)
     pyxel.rect(camera.xOnScreen(room.player.xpos),camera.yOnScreen(room.player.ypos),room.player.length,room.player.height,3)
-    if room.player.is_attacking:
+    if room.player.attacking:
         pyxel.rect(camera.xOnScreen(room.player.attack.xpos),camera.yOnScreen(room.player.attack.ypos),room.player.attack.length,room.player.attack.height,14)
     
     for i in range (len(room.walls_hitboxes)):
         pyxel.rect(camera.xOnScreen(room.walls_hitboxes[i].xpos),camera.yOnScreen(room.walls_hitboxes[i].ypos),room.walls_hitboxes[i].length,room.walls_hitboxes[i].height,4)
     
+    if room.player.hp <= 0 :
+        pyxel.cls(0)
+        pyxel.text(config["game"]["width"]/2,config["game"]["height"]/2 ,"GAME OVER",7)
+        pyxel.text(config["game"]["width"]/2,config["game"]["height"]/3 ,"you died",7)
 pyxel.run(update, draw)
