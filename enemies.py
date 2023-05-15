904368c80d450576e88ec2d8dcb419e4875b6109
Elisa N <117632231+iminiom@users.noreply.github.com>
2023-05-15 09:49:15 +0200

    added whizard

--- enemies.py
+++ enemies.py
@@ -63,21 +63,24 @@ class Test_enemy(gameobjects.Entity):
 
       self.hitbox.moveTo(round(self.xpos),round(self.ypos))
 
+   def mouvement(self,player):
+     print("ye")
+
+   def draw(self,x,y):
+        pyxel.rect(x,y,self.length,self.height,4)
+        
+
 class Bat(gameobjects.Entity):
-     def __init__(self, x, y, walls_list, speedx = 0, speedy = 0, facing_right = True):
-         super().__init__(x,y,8,8,5,walls_list)#mettre les valeur manuellement
-         self.xspeed = 0.7  
-
-     class Bat(gameobjects.Entity):
-      def init(self, x, y, walls_list, speedx = 0, speedy = 0, facing_right = True):
-            super().init(x,y,8,8,5,walls_list)#mettre les valeur manuellement
+      
+      def __init__(self, x, y, walls_list, speedx = 0, speedy = 0, facing_right = True):
+            super().__init__(x,y,8,8,5,walls_list)#mettre les valeur manuellement
             self.xspeed = 0.7
-
+      
       def mouvement(self,player):
 
-            if (player.xpos - self.xpos)**2 + (player.ypos - self.ypos)**2 <= 2304: 
-                  angle_to_player = math.degrees((self.ypos-player.ypos)/(self.xpos-player.xpos))
-                  self.addSpeedWithAngle(angle_to_player,0.25)
+            if (player.xpos - self.xpos)**2 + (player.ypos - self.ypos)**2 <= 2304:
+                  angle_to_player = math.atan2(self.ypos-player.ypos,self.xpos-player.xpos)
+                  self.setSpeedWithAngle(angle_to_player,-0.5)  
 
             else:
                   if self.xspeed > 0:
@@ -89,3 +92,44 @@ class Bat(gameobjects.Entity):
                         self.yspeed -= 0.25
                   elif self.yspeed < 0:
                         self.yspeed += 0.25
+      def draw(self,x,y):
+            pyxel.rect(x,y,self.length,self.height,1)
+
+class Whizard(gameobjects.Entity):
+     
+     def __init__(self, x, y, wall_list, speedx = 0, speedy =0):
+          super().__init__(x,y,8,8,5, wall_list)#mettre les valeur manuellement
+          self.xspeed = 0.7
+          facing_player = False
+          edge_right = False
+          edge_left = False
+          blindness_time = 0 
+
+     def mouvement(self,player):      
+            if (player.xpos - self.xpos)**2 + (player.ypos - self.ypos)**2 <= 2304:
+               facing_player = True 
+
+            if facing_player == True and self.xpos > player.xpos:
+                  self.xspeed += 0.01
+            elif facing_player == True and self.xpos < player.xpos:
+                  self.xspeed -= 0.01
+            
+            else:
+               
+               for hb in self.walls_list:
+                    if not hb.is_point_in(self.xpos-1,self.ypos+self.height) and hb.is_point_in(self.xpos,self.ypos+self.height):
+                        self.edge_left = True
+                    if not hb.is_point_in(self.xpos+self.length+1,self.ypos+self.height) and hb.is_point_in(self.xpos+self.length,self.ypos+self.height):
+                        self.edge_right = True
+
+               if self.edge_left == True or self.edge_right == True:
+                    self.xspeed = -self.xspeed
+
+     def attack(self, player):     
+          if self.xpos == player.xpos: 
+               while self.blindness_time > 0:
+                    pyxel.camera(30,20)
+
+     def draw(self,x,y):
+            pyxel.rect(x,y,self.length,self.height,)
+
